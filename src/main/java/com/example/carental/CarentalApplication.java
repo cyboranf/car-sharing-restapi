411dfbdf802e4c2161f38aa3952ce6f2cd2b6e45
cyboranf <cyboranf@gmail.com>
2023-05-14 15:16:05 +0200

    project init

---
+++ src/main/java/com/example/carental/CarentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.carental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carental/CarentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
