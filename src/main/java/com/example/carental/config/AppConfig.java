702d90cdfd5f56de7fc206400f78ebe0a6b6d1bb
cyboranf <cyboranf@gmail.com>
2023-05-24 15:55:38 +0200

    config -change

---
+++ src/main/java/com/example/carental/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.example.carental.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.carental")
+public class AppConfig {
+
+}
