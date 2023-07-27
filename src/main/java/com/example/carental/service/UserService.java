509649a0565ace5bcc28948542e09b39d49b8ded
cyboranf <cyboranf@gmail.com>
2023-07-27 14:04:15 +0200

    clean code

--- src/main/java/com/example/carental/service/UserService.java
+++ src/main/java/com/example/carental/service/UserService.java
@@ -15,6 +15,7 @@ import com.example.carental.validation.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import javax.transaction.Transactional;
 import java.util.Collections;
 import java.util.List;
